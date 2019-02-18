5e2fc42bb54eed8d8e107fd0bf8e4f2bebb9e86c
chuxiaoyi <chuxiaoyi@mioji.com>
2019-02-18 10:22:09 +0800

    添加demo

---
+++ tornado_S/demo1/hello.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------
+#       @Time    : 2019/1/28 上午11:43
+#       @Author  : cxy =.= 
+#       @File    : hello-errors.py
+#       @Software: PyCharm
+#       @Desc    : 
+# --------------------------------------
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+
+define("port", default=8000, help="run on the given port", type=int)
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        greeting = self.get_argument('greeting', 'Hello')
+        self.write(greeting + ', friendly user!')
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers=[
+            (r"/", IndexHandler)
+        ]
+    )
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
